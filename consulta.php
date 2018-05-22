3756f5709d85985f663e23d7fa6941151b9bf2cb
kpadillar <36040892+kpadillar@users.noreply.github.com>
2018-05-22 08:42:07 -0500

    Create consulta.php
    Examen 3

---
+++ consulta.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset ="UTF-8">
+    <script src = "js/jquery-3.3.1.js"></script>
+    <script src = "js/peticion_ajax.js"></script>
+</head>
+
+<body>
+    
+</body>
+
+</html>
