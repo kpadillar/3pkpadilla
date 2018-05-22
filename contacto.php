1e46f4597161a909ff1d1e9b364c023c99b31235
kpadillar <36040892+kpadillar@users.noreply.github.com>
2018-05-22 08:52:10 -0500

    Create contacto.php
    Examen 3

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+<head>
+  <meta charset="UTF-8"/>
+  <title>Contáctanos</title>
+  <link href="registro.css" rel="stylesheet">
+ 
+</head>
+
+<body>
+    <h2>Por favor mándanos tus comentarios</h2>
+    <a href="mailto:3pkpadilla@localhost.unam.mx" subject=Comentario">Enviar</a>
+
+</body>
+
+<footer>
+    
+</footer>
+
+</html>
