4246dc801a7ab13c1f4e916cd372b7d2cd16cc38
kpadillar <36040892+kpadillar@users.noreply.github.com>
2018-05-22 08:38:13 -0500

    creditos.php
    Examen 3

---
+++ creditos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <link rel="stylesheet" href="registro.css">
+    <img src="/imagenes/unam.jpg" alt="UNAM">
+    <img src="/imagenes/fca.jpg" alt="FCA">
+</head>
+
+<div class="container texto_contenido" style="text-align:center;">
+		<div class="fancy-title title-border" align="center">
+			<h3>Créditos</h3>
+		</div>
+		<strong>Mtro. Tomás Humberto Rubio Pérez</strong>
+		<br>
+		"Director de la Facultad de Contaduría y Administración"
+		<br>
+		<br>
+		<strong>Dr. Armando Tomé González</strong>
+		<br>
+		"Secretario General"
+		<br>
+		<br>
+		<strong>Mtro. Balfred Santaella Hinojosa</strong>
+		<br>
+		"Jefe del centro de Informática"
+		<br>
+		<br>
+		<strong>Karen Monserrat Padilla Rojas</strong>
+		<br>
+		"Responsable de Diseño y Desarrollo Web"
+		<br>
+		<br>
+	</div>
+  
+  </html>
