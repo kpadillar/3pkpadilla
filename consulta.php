e572479c75515fa9d51e254a371264c369aedd51
kpadillar <36040892+kpadillar@users.noreply.github.com>
2018-05-22 09:10:34 -0500

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -7,8 +7,19 @@
     <script src = "js/peticion_ajax.js"></script>
 </head>
 
+<?php
+include_once("conexion.php");
+	
 <body>
-    
+    $psql=psql_query("SELECT * FROM usuarios",$con);
+
+	echo "</p>Usuarios</p> \n";
+	while($row = mysql_fetch_array($sql)){
+		echo "<p>".$row['usuario']." - ".$row['ap_paterno']." - ".$row['ap_materno']."</p> \n";
+	}
+	
+?>
+	
 </body>
     
 <footer>
