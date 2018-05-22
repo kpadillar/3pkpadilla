ce2bebe5202caca5defc35d473b30c523613ce6a
kpadillar <36040892+kpadillar@users.noreply.github.com>
2018-05-22 08:21:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,17 @@
 </head>
 
 <body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+  	var js, fjs = d.getElementsByTagName(s)[0];
+  	if (d.getElementById(id)) return;
+  		js = d.createElement(s); js.id = id;
+  		js.src = 'https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0';
+  		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script
+	
+	<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FUNAM.MX.Oficial%2F&width=450&layout=standard&action=like&size=small&show_faces=true&share=true&height=80&appId" width="450" height="80" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>	
+		
 	<h2>Formulario registro</h2>
 	
 	<h2>"Registro de Usuarios"</h2>
@@ -30,4 +41,7 @@
 		
 </body>
 
+<footer>
+	<div class="fb-like" data-href="https://www.facebook.com/UNAM.MX.Oficial/" data-layout="standard" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+</footer>
 </html>
